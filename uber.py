ad352cfc0b7c439a9fb35496b9f77e873a739251
Jackson Davis <jdavis3695@gmail.com>
2022-01-25 11:47:58 -0500

    updated

--- uber.py
+++ uber.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from typing import Union
 
 data_file = pd.read_csv('cab_rides.csv')
 
@@ -8,10 +9,34 @@ def getdict(column: str):
     dictionary = {}
     for i in data:
         if i not in dictionary:
+            print(i)
             dictionary[i] = 1
         else:
             dictionary[i] += 1
     return dictionary
 
-destinations = getdict('cab_type')
-print(destinations)
+#destinations = getdict('destination')
+#print(destinations)
+
+def getavg(column: Union[int, float]):
+    if isinstance(column, int) == True:
+        print("int")
+
+getavg(1)
+
+
+def isolate_data(column: str, value: str):
+    data_2 = data_file.loc[data_file[column] == value]
+    return data_2
+
+
+isolated_data = isolate_data('destination', 'North Station')
+
+print(isolated_data)
+
+
+
+
+
+    
+  
